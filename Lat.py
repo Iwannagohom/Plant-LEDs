60efe00b104b28ae4e65c9c89c0a56ca864245b9
Iwannagohom <113135934+Iwannagohom@users.noreply.github.com>
2023-05-23 22:30:55 -0500

    Update Lat.py
    With Comments

--- Lat.py
+++ Lat.py
@@ -8,7 +8,7 @@ import json
 
 
 #---Conect to Wifi----
-wifi.radio.connect("TFS Students", "Fultoneagles")          #Name, Pasword
+wifi.radio.connect("TFS Students", "Fultoneagles")          #Name and Pasword of the wifi
 print("connected")
 
 pool = socketpool.SocketPool(wifi.radio)
@@ -18,9 +18,11 @@ print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
 print("My IP address is", wifi.radio.ipv4_address)
 
 
-
+#----Url to get the time of sunset and sunrise---
 url = "https://api.sunrise-sunset.org/json?lat=38.6631&lng=-90.5771"
-
+                                #Lat and Lng is latitude and longitude.
+                                #The time of a specific place can be found by puting in the cordinats of that place.
+                    
 response = requests.get(url)
 print("!!!!!!!!!!",response.text)
 
@@ -33,7 +35,7 @@ for key in t.keys():
     
 print("			")
 
-rise = (t["results"]["sunrise"])
+rise = (t["results"]["sunrise"]).    #Spesificly looking for sunset and sunset within the big chunk of text
 setd = (t["results"]["sunset"])
 print(rise, setd)
 print("			")
